75ebd571594553608f1e05980411781ef19cbb25
beckrw <beckrw@users.noreply.github.com>
2015-02-17 15:49:55 -0500

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,27 +1,33 @@
 complete <- function(directory, id = 1:332) {
-    
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
-    
-    ## 'id' is an integer vector indicating the monitor ID numbers
-    ## to be used
-    
-    ## Returns a data frame of the form:
-    ## id nobs
-    ## 1  117
-    ## 2  1041
-    ## ...
-    ## where 'id' is the monitor ID number and 'nobs' is the
-    ## number of complete cases
-    
-    files <- (Sys.glob("specdata//*.csv"));
-    nobs <- c();
-    
-    for (index in id) {
-        file_data <- read.csv(files[index], sep = ",");
-        complete_cases <- file_data[complete.cases(file_data),];
-        nobs <- c(nobs, nrow(complete_cases));
-    }
-    
-    return(data.frame(cbind(id, nobs)));
+
+## 'directory' is a character vector of length 1 indicating
+## the location of the CSV files
+## 'id' is an integer vector indicating the monitor ID numbers
+## to be used
+## Return a data frame of the form:
+## id nobs
+## 1 117
+## 2 1041
+## ...
+## where 'id' is the monitor ID number and 'nobs' is the
+## number of complete cases
+
+nobs			<- c()
+
+for(id_curr in id) {
+	if(id_curr <= 9) {
+		ID_As_Str 		<- paste('00', id_curr, sep='')	
+	} else if(id_curr <= 99) {
+		ID_As_Str 		<- paste('0', id_curr, sep='')
+	} else {
+		ID_As_Str 		<- paste(id_curr)
+	}
+
+	File_Path 		<- paste(directory, '/', ID_As_Str, '.csv', sep='')
+	Data 			<- read.csv(File_Path, header=T)
+	nobs 			<- c(nobs, sum(complete.cases(Data)))
+}
+
+Result		<- data.frame(id, nobs)
+
 }
